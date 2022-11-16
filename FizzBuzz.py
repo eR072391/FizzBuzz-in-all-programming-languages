ca2176351480a2175be6fbe083f77e6acf22640a
er072391 <76501872+eR072391@users.noreply.github.com>
2022-11-16 13:21:01 +0900

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+def main():
+        for i in range(100):
+            if i % 3 == 0 and i % 5 == 0:
+                print(i,"FizzBuzz.")
+            elif i % 3 == 0:
+                print(i,"Fizz.")
+            elif i % 5 == 0:
+                print(i,"Buzz.")
+            else:
+                print(i)
+        
+
+if __name__ == '__main__':
+    main()
